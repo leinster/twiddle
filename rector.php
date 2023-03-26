bf607b8f119d3d7e0215e7b636a2d0c6f6f49fbf
Jonathon Ramsey <jonathon.ramsey@gmail.com>
2023-03-26 16:31:47 +0100

    Enable rector SetList::PSR_4
    But skip the rule that modifies the namespace in the functions file.

--- rector.php
+++ rector.php
@@ -1,11 +1,11 @@
 <?php
-
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+
+use Rector\PSR4\Rector\FileWithoutNamespace\NormalizeNamespaceByPSR4ComposerAutoloadRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . "/src", __DIR__ . "/tests"]);
@@ -17,8 +17,14 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::DEAD_CODE,
         SetList::NAMING,
         SetList::PRIVATIZATION,
-        // SetList::PSR_4,
+        SetList::PSR_4,
         SetList::TYPE_DECLARATION,
         SetList::EARLY_RETURN,
+    ]);
+
+    $rectorConfig->skip([
+        NormalizeNamespaceByPSR4ComposerAutoloadRector::class => [
+            __DIR__ . "/src/functions.php",
+        ],
     ]);
 };
