8d16a4580a5c0fce711760b199d76befe9bcd375
Jonathon Ramsey <jonathon.ramsey@gmail.com>
2024-12-31 14:27:08 +0000

    Update rector config

--- rector.php
+++ rector.php
@@ -2,20 +2,20 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . "/src", __DIR__ . "/tests"]);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::DEAD_CODE,
-        SetList::NAMING,
-        SetList::PRIVATIZATION,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-    ]);
-};
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . "/src", __DIR__ . "/tests"])
+    ->withPhpSets(php82: true)
+    ->withPreparedSets(
+        codeQuality: true,
+        codingStyle: true,
+        carbon: true,
+        deadCode: true,
+        earlyReturn: true,
+        instanceOf: true,
+        naming: true,
+        privatization: true,
+        rectorPreset: true,
+        strictBooleans: true,
+        typeDeclarations: true
+    );
