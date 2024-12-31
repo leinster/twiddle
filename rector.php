d3416345578a34fda39984ade0bf23c3e23477de
Jonathon Ramsey <jonathon.ramsey@gmail.com>
2024-12-31 14:02:23 +0000

    Update rector config
    This needs another look based on current docs.

--- rector.php
+++ rector.php
@@ -5,8 +5,6 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
-use Rector\PSR4\Rector\FileWithoutNamespace\NormalizeNamespaceByPSR4ComposerAutoloadRector;
-
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . "/src", __DIR__ . "/tests"]);
 
@@ -17,14 +15,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::DEAD_CODE,
         SetList::NAMING,
         SetList::PRIVATIZATION,
-        SetList::PSR_4,
         SetList::TYPE_DECLARATION,
         SetList::EARLY_RETURN,
-    ]);
-
-    $rectorConfig->skip([
-        NormalizeNamespaceByPSR4ComposerAutoloadRector::class => [
-            __DIR__ . "/src/functions.php",
-        ],
     ]);
 };
